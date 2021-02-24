6b9f8289287139783195718a842181064603cf08
Guillaume Bisiaux <guillaume@heroes.jobs>
2021-02-24 10:38:09 +0100

    Update Framework

--- heroes_rainbow/StoryBook Extensions/ItemActionExtension.swift
+++ heroes_rainbow/StoryBook Extensions/ItemActionExtension.swift
@@ -18,8 +18,14 @@ extension HomeViewController {
         let deleteCell = ItemAction()
         deleteCell.setupAsDelete(text: "Delete label 🗑")
         
+        let customCell = ItemAction()
+        customCell.setupAsCustomButton(text: "Add work experience", color: R.color.purple()!)
+        
         let experienceCell = ItemActionLarge()
-        experienceCell.setup(title: "Barista", subtitle: "Jack in the box", info: "3 months")
+        experienceCell.setup(title: "Barista", subtitle: "Jack in the box", info: "3 months", type: .pushLeft)
+         
+        let asBtnCell = ItemActionLarge()
+        asBtnCell.setup(title: "Barista", subtitle: "Jack in the box", info: "3 months", type: .button)
         
         let vc = StoryBookExampleViewController(
             title: "Item Action",
@@ -28,8 +34,11 @@ extension HomeViewController {
                 .init(title: "Unactive item", view: unableCell),
                 .init(title: "Active item with info", view: infoCell),
                 .init(title: "Delete item", view: deleteCell),
+                .init(title: "Button item", view: customCell),
                 .init(title: "Link item", view: linkCell),
-                .init(title: "Large item", view: experienceCell)
+                .init(title: "Large item", view: experienceCell),
+                .init(title: "Large item as a button", view: asBtnCell),
+
             ])
         self.navigationController?.pushViewController(vc, animated: true)
     }
